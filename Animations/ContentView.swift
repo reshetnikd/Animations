296c27fa6e4173ab7965b60993db1a7304937713
reshetnikd <dmitry.reshetnik@icloud.com>
2020-01-29 23:07:26 +0200

    Add button and try out some animations code

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,8 +9,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    @State private var animationAmount: CGFloat = 1
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        Text("Hello, World!")
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        
+//        Button("Tap Me") {
+//            //self.animationAmount += 1
+//        }
+//        .padding(50)
+//        .background(Color.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        //.scaleEffect(animationAmount)
+//        //.animation(.interpolatingSpring(stiffness: 50, damping: 1))
+//        //.blur(radius: (animationAmount - 1) * 3)
+//        .overlay(
+//            Circle()
+//                .stroke(Color.red)
+//                .scaleEffect(animationAmount)
+//                .opacity(Double(2 - animationAmount))
+//                .animation(
+//                    Animation.easeInOut(duration: 1)
+//                        //.delay(1)
+//                        //.repeatCount(3, autoreverses: true)
+//                        .repeatForever(autoreverses: false)
+//                )
+//        )
+//        .onAppear {
+//            self.animationAmount = 2
+//        }
+//        print(animationAmount)
+//
+//        return VStack {
+//            Stepper("Scale amount", value: $animationAmount.animation(
+//                Animation.easeInOut(duration: 1)
+//                    .repeatCount(3, autoreverses: true)
+//            ), in: 1...10)
+//
+//            Spacer()
+//
+//            Button("Tap Me") {
+//                self.animationAmount += 1
+//            }
+//            .padding(40)
+//            .background(Color.red)
+//            .foregroundColor(.white)
+//            .clipShape(Circle())
+//            .scaleEffect(animationAmount)
+//        }
     }
 }
 

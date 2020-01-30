1b37b05bdd901ffc29e43100d4de2fecf04c5ace
reshetnikd <dmitry.reshetnik@icloud.com>
2020-01-30 19:13:24 +0200

    Try out some code with animation stack

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,65 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     
-//    @State private var animationAmount: CGFloat = 1
-    @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
         Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                self.animationAmount += 360
-            }
+            self.enabled.toggle()
         }
-        .padding(50)
-        .background(Color.red)
+        .frame(width: 200, height: 200)
+        .background(enabled ? Color.blue : Color.red)
+        .animation(.default) // can be nil
         .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
-        
-//        Button("Tap Me") {
-//            //self.animationAmount += 1
-//        }
-//        .padding(50)
-//        .background(Color.red)
-//        .foregroundColor(.white)
-//        .clipShape(Circle())
-//        //.scaleEffect(animationAmount)
-//        //.animation(.interpolatingSpring(stiffness: 50, damping: 1))
-//        //.blur(radius: (animationAmount - 1) * 3)
-//        .overlay(
-//            Circle()
-//                .stroke(Color.red)
-//                .scaleEffect(animationAmount)
-//                .opacity(Double(2 - animationAmount))
-//                .animation(
-//                    Animation.easeInOut(duration: 1)
-//                        //.delay(1)
-//                        //.repeatCount(3, autoreverses: true)
-//                        .repeatForever(autoreverses: false)
-//                )
-//        )
-//        .onAppear {
-//            self.animationAmount = 2
-//        }
-//        print(animationAmount)
-//
-//        return VStack {
-//            Stepper("Scale amount", value: $animationAmount.animation(
-//                Animation.easeInOut(duration: 1)
-//                    .repeatCount(3, autoreverses: true)
-//            ), in: 1...10)
-//
-//            Spacer()
-//
-//            Button("Tap Me") {
-//                self.animationAmount += 1
-//            }
-//            .padding(40)
-//            .background(Color.red)
-//            .foregroundColor(.white)
-//            .clipShape(Circle())
-//            .scaleEffect(animationAmount)
-//        }
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
     }
 }
 

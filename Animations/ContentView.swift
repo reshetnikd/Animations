3328353d50fe7c08d244c0466e75a9e2e1fab0ec
reshetnikd <dmitry.reshetnik@icloud.com>
2020-01-30 19:36:48 +0200

    Add drag gesture with animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,18 +10,47 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let letters = Array("Hello SwiftUI")
     @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        Button("Tap Me") {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num) / 20))
+            }
+        }
+    .gesture(
+        DragGesture()
+            .onChanged() {
+                self.dragAmount = $0.translation
+        }
+        .onEnded() { _ in
+            self.dragAmount = .zero
             self.enabled.toggle()
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? Color.blue : Color.red)
-        .animation(.default) // can be nil
-        .foregroundColor(.white)
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+    )
+//        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 300)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//            .offset(dragAmount)
+//            .gesture(
+//                DragGesture()
+//                .onChanged {
+//                    self.dragAmount = $0.translation
+//                }
+//                .onEnded { _ in
+//                    withAnimation(.spring()) {
+//                        self.dragAmount = .zero
+//                    }
+//                }
+//            )
+//            //.animation(.spring())
     }
 }
 
